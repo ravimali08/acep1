55f12675757458ff54642011c802ff48ac388a8d
sujal <sujal@example.com>
2026-06-30 11:48:58 +0530

    build: install and configure vite-plugin-image-optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig(async ({ mode }) => {
-  const plugins = [react(), tailwindcss()];
+  const plugins = [
+    react(), 
+    tailwindcss(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      jpg: { quality: 80 },
+      webp: { quality: 80 },
+    })
+  ];
   try {
     // @ts-expect-error - optional dev plugin hook
     const m = await import('./.vite-source-tags.js');
